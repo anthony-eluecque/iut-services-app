7b15e7e321ebd263b97641846a9855d2863a69e6
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-07 20:12:46 +0200

    Fix: Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,21 +2,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
-  },
-]
+  
+    {
+      path: '/home',
+      name: 'Home',
+      component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+    },
+    {
+      path: '/services',
+      name : 'Services',
+      component: () => import(/* webpackChunkName: "home" */ '@/views/Services.vue'),
+    }
+  ]
+
+
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
