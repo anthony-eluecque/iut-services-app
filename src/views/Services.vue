c3896d50dba180635359fb39889e2b5b50eec4e7
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-08 00:29:05 +0200

    Feat: Services page (first improve)

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,14 +1,39 @@
 <template>
-  <section class="container">
+  <section class="container d-flex flex-column">
     <div class="container-title d-flex">
       <h2>Liste des Services prévisionnels</h2>
       <h2>logo</h2>
+    </div>
+
+    <div class="container-actions d-flex">
+      <div class="add-action">
+        <v-btn
+            @click="addInputFields" 
+            class="d-flex"
+            height="55px"
+            append-icon="mdi-plus"
+            text="Ajouter un enseignement"
+            color="primary"
+        />
+      </div>
+    </div>
+
+    <div class="container-content">
+      <DataTable />
     </div>
   </section>
 </template>
   
 <script lang="ts" setup>
-  
+import DataTable from '@/components/DataTable.vue'
+import { useAppStore } from '@/store'
+
+const AppStore = useAppStore();
+
+const addInputFields = () => {
+    AppStore.editIsCreatingItem(true)
+}
+
 </script>
   
 <style>
@@ -19,7 +44,19 @@
     display: flex;
     padding: 20px;
     background-color: #F4F3F3;
-    align-items: start;
+    justify-content: space-around;
+}
+
+.container-content {
+    /* background-color: white; */
+    width: 100%;
+    height: 50%;
+}
+
+.container-actions{
+    height: 8%;
+    justify-content: flex-end;
+    align-items: center;
 }
 
 .container-title{
@@ -36,6 +73,10 @@
     font-family: 'Montserrat',sans-serif;
     font-weight: bold;
     font-size: 32px;
+}
+
+.add-action{
+    display: flex;
 }
 
 </style>
