7a245515e7a36298d9569f687c09b864ebe7c756
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-07 19:15:52 +0200

    Pkg: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,10 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    'plugin:vue/recommended'
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
+    'indent': ['error', 2],
   },
 }
