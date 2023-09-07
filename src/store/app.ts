bc3c6124aa42a9924691f284f12cf2cf693a7c03
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-08 00:28:45 +0200

    Feat: Store pinia

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,24 @@
 // Utilities
 import { defineStore } from 'pinia'
 
+// Utile pour typer son store
+interface RootState {
+  isCreatingItem : boolean
+}
+
+// Méthode d'appel pour initialiser le store
+const initStore = () : RootState => {
+  return {
+    isCreatingItem : false
+  }
+}
+
 export const useAppStore = defineStore('app', {
-  state: () => ({
-    //
-  }),
+  state: () : RootState => initStore(),
+  getters:{}, // Getters => transformations nécessaire avant d'être utiliser dans le code (pas forcément nécessaire dans un premier temps)
+  actions:{ // Actions => changer un état => une méthode, JAMAIS CHANGER EN DEHORS DE CES METHODES IMPORTANT
+    editIsCreatingItem(newState : boolean) {
+      this.isCreatingItem = newState;
+    }
+  }
 })
