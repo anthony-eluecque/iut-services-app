1e249b20d7eedb55b9040642671ded9844fe2c00
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-07 20:12:57 +0200

    Feat: views (Home, services)

---
+++ src/views/Services.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+    <div class="container-title d-flex">
+      <h2>Liste des Services prévisionnels</h2>
+      <h2>logo</h2>
+    </div>
+  </section>
+</template>
+  
+<script lang="ts" setup>
+  
+</script>
+  
+<style>
+.container{
+    width: 95%;
+    margin: auto;
+    height: 100vh;
+    display: flex;
+    padding: 20px;
+    background-color: #F4F3F3;
+    align-items: start;
+}
+
+.container-title{
+    padding-left: 20px;
+    padding-right: 20px;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 110px;
+    background: white;
+}
+
+.container-title h2{
+    font-family: 'Montserrat',sans-serif;
+    font-weight: bold;
+    font-size: 32px;
+}
+
+</style>
