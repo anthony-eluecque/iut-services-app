b1d5368f4f4050b4083ed707e49f8a4d3cd4e190
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-09-08 19:59:35 +0200

    Imp : App Store

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,15 +1,34 @@
 // Utilities
+import { InputFieldType } from '@/types'
 import { defineStore } from 'pinia'
 
-// Utile pour typer son store
 interface RootState {
   isCreatingItem : boolean
+  inputField : InputFieldType
+}
+
+const initInputField = () : InputFieldType => {
+  return {
+    lesson : {
+      amountHours : 0,
+      givenId : "",
+      Id : "",
+      label : ""
+    },
+    teacher : {
+      firstname : "",
+      givenId : "",
+      Id : "",
+      lastname : ""
+    }
+  }
 }
 
 // Méthode d'appel pour initialiser le store
 const initStore = () : RootState => {
   return {
-    isCreatingItem : false
+    isCreatingItem : false,
+    inputField : initInputField()
   }
 }
 
@@ -19,6 +38,14 @@ export const useAppStore = defineStore('app', {
   actions:{ // Actions => changer un état => une méthode, JAMAIS CHANGER EN DEHORS DE CES METHODES IMPORTANT
     editIsCreatingItem(newState : boolean) {
       this.isCreatingItem = newState;
+    },
+    setInputFieldValue<K extends keyof InputFieldType, F extends keyof InputFieldType[K]>(
+      objName: K,
+      fieldName: F,
+      newValue: InputFieldType[K][F]
+    ) {
+      this.inputField[objName][fieldName] = newValue;
     }
+    
   }
 })
