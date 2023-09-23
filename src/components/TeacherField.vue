230d70e25a41c760e2ab5606d366d0da700c7339
laebernard <97946771+laebernard@users.noreply.github.com>
2023-09-23 16:49:48 +0200

    Imp: TeacherField
    vitalization of teachers' information fields

--- src/components/TeacherField.vue
+++ src/components/TeacherField.vue
@@ -1,77 +1,66 @@
 <template>
-        <div class ="container-info d-flex">
-            <!-- <tr
-                v-for="item in dataRowsById"
-                :key="item.id"
-            > -->
-       
+  <div class ="container-info d-flex">
+   
+    <tr v-for="item in dataRows" :key="item.id">
+
+      <td>
+        <v-text-field
+          :value="item.service?.teacher?.firstName"
+          variant="solo"
+          readonly
+        ></v-text-field>
+      </td>
         
+      <td>
+        <v-text-field
+          :value="item.service?.teacher?.lastName"
+          variant="solo"
+          readonly
+        ></v-text-field>
+      </td>
 
-
+          <td>
             <v-text-field
-            model-value="John"
+              :value="item.service?.teacher?.givenId"
+              variant="solo"
+              readonly
+            ></v-text-field>
+          </td>
+        </tr>
+     
+  </div>
 
-            variant="solo"
-            readonly
-          ></v-text-field>
-          <v-text-field
-            model-value="Doe"
-            variant="solo"
-      
-            readonly
-          ></v-text-field>
-          <v-text-field
-            model-value="1"
-            variant="solo"
-            readonly
-          ></v-text-field>
-       
-        <!-- </tr> -->
-        </div>
-    
 </template>
 
 <script lang="ts" setup>
 import { useAppStore } from '@/store'
-import InputField from './InputField.vue';
 import { computed, onMounted, ref} from 'vue';
-import { Item } from '@/types';
-import router from '@/router';
 
 const AppStore = useAppStore();
 
-const dataRowsById = computed(() => AppStore.getDataRows);
-
-const teacherId = router.currentRoute.value.params.id
+const dataRows = computed(() => AppStore.getDataRows);
 
 </script>
 
 <style>
 
 .container-info{
-    padding-left: 20px;
-    padding-right: 20px;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 110px;
-    background: white;
+  padding-left: 20px;
+padding-right: 20px;
+justify-content: space-between;
+align-items: center;
+width: 100%;
+height: 110px;
+background: white;
 }
 
-.container-info v-text-field{
+.container-info td{
 
+   padding-right: 10%;
+  display: inline-block; 
+  width: 300px;
 
-    padding-left: 20px;
-    padding-right: 20px;
-    border: 1px solid black;
-    padding-top: 1%;
-    padding-bottom: 1%;
 }
 
-.v-input__control {
-  display: flex;
-  grid-area: control;
-  margin-left: 5%;
-}
 
 </style>
