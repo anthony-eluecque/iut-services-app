2af32d37114c8dd8d886624c8ad59379013e490a
laebernard <97946771+laebernard@users.noreply.github.com>
2023-09-29 09:35:40 +0200

    Imp: Details.vue
    Creating a pdf of a Preview Service

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,4 +1,5 @@
 <template>
+
     <section class="container d-flex flex-column justify-space-between">
         <div class="container-title d-flex">
             <h2>Visualisation d'un Service Prévisionnel</h2>
@@ -23,8 +24,11 @@
             <v-btn prepend-icon="mdi-arrow-left" text="Retour" color="red" @click="returnServicePage()" />
             <v-btn prepend-icon="mdi-download" text="Télécharger en PDF" color="green" @click="downloadAsPDF()" />
         </div>
+       
     </section>
 </template>
+
+
 
   
 <script lang="ts" setup>
@@ -36,6 +40,8 @@ import router from '@/router';
 import { Routes, fetchData, extractData } from '@/api';
 import { Item, Teacher } from '@/types';
 import { Service } from '@/types/service.types';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 
 const selectedItem: Ref<Item | null> = ref(null)
@@ -57,27 +63,84 @@ onBeforeMount(async () => {
     selectedService.value = extractData(await fetchData(Routes.SERVICES + `/${selectedService.value.id}`))
 })
 
+console.log(selectedItem.value?.lesson?.givenId)
+
 const returnServicePage = () => {
     router.push('/services/')
 }
 
 const downloadAsPDF = () => {
-    // this.refs.Vue3Html2pdf.generatePdf();
 
-    console.log("pdf")
+    const doc = new jsPDF({ unit: 'pt', format: 'a4' });
 
-    const doc = new jsPDF({
-        orientation:"portrait",
-        unit: "px",
-        format: "a4"
-    });
 
-    doc.save(`Service.pdf`);
+    // header
+
+const leftText = `Année Universitaire: ${selectedService.value.year}-${selectedService.value.year + 1}`;
+const rightText = `Service prévisionnel de ${selectedTeacher.value.lastName} ${selectedTeacher.value.firstName}`;
+
+const headerBlockX = 50;
+const headerBlockY = 50;
+const headerBlockWidth = doc.internal.pageSize.getWidth() - headerBlockX * 2;
+const headerBlockHeight = 40;
+
+doc.rect(headerBlockX, headerBlockY, headerBlockWidth, headerBlockHeight);
+
+doc.setFontSize(12);
+
+const rightTextWidth = doc.getTextWidth(rightText);
+const rightTextX = headerBlockX + headerBlockWidth - rightTextWidth - 10; // 10 is a padding from the right edge
+
+doc.text(leftText, headerBlockX + 10, headerBlockY + headerBlockHeight / 2 + 3); // The +3 is to adjust the vertical centering of the text
+doc.text(rightText, rightTextX, headerBlockY + headerBlockHeight / 2 + 3); // The +3 is to adjust the vertical centering of the text
+
+//  semestre
+
+const id = selectedItem.value?.lesson?.givenId ? selectedItem.value?.lesson?.givenId : ""
+
+
+const Semestre = parseInt(id.split('.')[0].slice(1));
+
+const CenterText = `Semestre ${Semestre}`;
+
+
+const Block1X = 50;
+const Block1Y = 125;
+const Bloc1kWidth = doc.internal.pageSize.getWidth() - Block1X * 2;
+const Block1Height = 40;
+
+doc.rect(Block1X, Block1Y, Bloc1kWidth, Block1Height);
+
+doc.setFontSize(12);
+const textWidth = doc.getTextWidth(CenterText);
+const centerX = Block1X + (Bloc1kWidth - textWidth) / 2;
+
+doc.text(CenterText, centerX, Block1Y + Block1Height / 2 + 3);
+
+
+// tableau semestre
+
+    const tableSemestres = [
+        ['Enseignements', 'Type', 'Volume'],
+        [selectedItem.value?.lesson?.givenId + ' ' + selectedItem.value?.lesson?.name, selectedItem.value?.type + ' 1h30', selectedItem.value?.amountHours],
+   
+];
+    const startY = 225; 
+
+
+const tablePosition = {
+  startY: startY, 
+};
+
+
+autoTable(doc,{
+  head: [tableSemestres[0]], 
+  body: tableSemestres.slice(1),
+  ...tablePosition, 
+});
+
+    doc.save('Service  ' +(selectedTeacher.value.lastName) + ' ' + (selectedTeacher.value.firstName) + (selectedService.value.year) + '-' +(selectedService.value.year + 1) +'.pdf');
 }
-
-
-
-
 
 </script>
 
