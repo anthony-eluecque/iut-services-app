e59a35b697310906d25698ddf67dde339dc5325f
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-10-02 15:46:15 +0200

    feat: teacher cards

---
+++ src/components/TeacherCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card width="400">
+        <template v-slot:title>
+            {{ props.teacher.firstName }} {{ props.teacher.lastName }}
+        </template>
+
+        <template v-slot:subtitle>
+            Professeur
+        </template>
+
+        <v-card-actions>
+            <v-btn>
+                Button
+            </v-btn>
+            <v-btn>
+                Button2
+            </v-btn>
+        </v-card-actions> </v-card>
+</template>
+
+
+<script setup lang="ts">
+import { Teacher } from '@/types';
+
+const props = defineProps({
+    teacher: {
+        type: Object as () => Teacher,
+        required: true
+    }
+})
+</script>
+
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
 <template>
   <h1>Home page</h1>
+  <TeacherCard :teacher="teacher" />
 </template>
     
 <script lang="ts" setup>
 
+import TeacherCard from '@/components/TeacherCard.vue';
+import { ref } from 'vue';
+
+const teacher = ref({
+  firstName: 'John',
+  lastName: 'Doe',
+  givenId: '123456789',
+  id: '1'
+});
 </script>
-    
