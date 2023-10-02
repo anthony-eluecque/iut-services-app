00f8a0a6cfb28e7e5319449d3f95d146f86bed5d
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-02 16:28:13 +0200

    Imp: Card's design

--- src/components/TeacherCard.vue
+++ src/components/TeacherCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card variant="outlined" width="400">
+    <v-card variant="outlined" width="300">
         <template v-slot:title>
             {{ props.teacher.firstName }} {{ props.teacher.lastName }}
         </template>
@@ -9,11 +9,11 @@
         </template>
 
         <v-card-actions>
-            <v-btn color="primary">
-                Button
+            <v-btn color="warning">
+                Modifier
             </v-btn>
-            <v-btn color="primary">
-                Button2
+            <v-btn color="error">
+                Supprimer
             </v-btn>
         </v-card-actions> </v-card>
 </template>
