29f8ca3bf4ce2c10b31fc27486e974d6279c678e
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-06 12:44:27 +0200

    Fix: Support switch between tabs

--- src/views/Directory.vue
+++ src/views/Directory.vue
@@ -4,61 +4,59 @@
         <h2>Répertoire</h2>
         <h2>logo</h2>
         </v-container>
-        {{  lessons }}
         <div class="container-content-tabs d-flex">
-            <v-card class="mb-4" >
+            <v-card>
                 <v-tabs
                 v-model="tab"
-                bg-color="primary"
+                color="primary"
+                align-tabs="start"
                 >
-                    <v-tab value="teachers">Professeurs</v-tab>
-                    <v-tab value="lessons">Enseignements</v-tab>
+                    <v-tab :value="1">Professeurs</v-tab>
+                    <v-tab :value="2">Leçons</v-tab>
                 </v-tabs>
-
-                <v-card-text>
-                    <v-window v-model="tab">
-                        <v-window-item class="" value="teachers">
+                <v-window v-model="tab">
+                    <v-window-item
+                        :value="1"
+                    >
+                        <v-container fluid>
                             <h1>Filtrer :</h1>
-                            <div class="mt-2 d-flex flex-row justify-space-between">
-                                <div class="pa-2 container-fields-filter flex-row d-flex">
-                                    <v-text-field
-                                        v-model="givenIdTeacher"
-                                        hide-details
-                                        label="Matricule Enseignant"
-                                        variant="outlined"
-                                    />
-                                    <v-text-field
-                                        v-model="firstnameTeacher"
-                                        hide-details
-                                        label="Prénom"
-                                        variant="outlined"
-                                    />
-                                    <v-text-field
-                                        v-model="lastnameTeacher"
-                                        hide-details
-                                        label="Nom"
-                                        variant="outlined"
-                                    />
+                                <div class="mt-2 d-flex flex-row justify-space-between">
+                                    <div class="pa-2 container-fields-filter flex-row d-flex">
+                                        <v-text-field
+                                            v-model="givenIdTeacher"
+                                            hide-details
+                                            label="Matricule Enseignant"
+                                            variant="outlined"
+                                        />
+                                        <v-text-field
+                                            v-model="firstnameTeacher"
+                                            hide-details
+                                            label="Prénom"
+                                            variant="outlined"
+                                        />
+                                        <v-text-field
+                                            v-model="lastnameTeacher"
+                                            hide-details
+                                            label="Nom"
+                                            variant="outlined"
+                                        />
+                                    </div>
+                                    <div class="pt-2 pb-2">
+                                        <v-btn
+                                            height="55px"
+                                            append-icon="mdi-plus"
+                                            text="Ajouter un professeur"
+                                            color="primary" 
+                                        />
+                                    </div>
                                 </div>
-                                <div class="pt-2 pb-2">
-                                    <v-btn
-                                        height="55px"
-                                        append-icon="mdi-plus"
-                                        text="Ajouter un professeur"
-                                        color="primary" 
-                                    />
+                                <div class="mt-12 teachers d-flex">
+                                    <TeacherCard class="teacher-grow" :teacher="t" v-for="(t,i) in teachers">test</TeacherCard>
                                 </div>
-                            </div>
-                            <div class="mt-12 teachers d-flex">
-                                <TeacherCard class="teacher-grow" :teacher="t" v-for="(t,i) in teachers">test</TeacherCard>
-                            </div>
-                        </v-window-item>
-
-                        <v-window-item class="lessons d-flex" value="lessons">
-                            <LessonsTab :lessons="lessons" />
-                        </v-window-item>
-                    </v-window>
-                </v-card-text>
+                        </v-container>
+                    </v-window-item>
+                    <LessonsTab :lessons="lessons" />
+                </v-window>
             </v-card>
         </div>
     </section>
@@ -79,7 +77,7 @@ const lessons : Ref<Lesson[]> = ref([]);
 const givenIdTeacher : Ref<string> = ref('');
 const firstnameTeacher : Ref<string> = ref('');
 const lastnameTeacher : Ref<string> = ref('');
-const tab = ref(0)
+const tab = ref("1")
 
 onBeforeMount(async() => {
     await AppStore.fetchTeachers()
