610855fe1e5d307f7cb4005dec85d21f4611f1f0
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-09 00:49:21 +0200

    Feat: split btnTheme in .ts and .vue

---
+++ src/components/btn-theme/btn-theme.component.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-btn @click="toggleTheme" class="v-btn--icon btn-theme">
+      <v-icon>{{theme.global.name.value == 'lightTheme' ? 'mdi-weather-night' : 'mdi-weather-sunny'}}</v-icon>
+    </v-btn>
+</template>
+
+<script setup lang="ts">
+import { toggleTheme, initializeTheme, theme } from './btn-theme.component'
+initializeTheme();
+</script>
+
+<style>
+/* .btn-theme{
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+} */
+</style>
