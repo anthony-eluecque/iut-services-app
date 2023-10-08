8c74fb87943e19bdf9a1670f4a7096cdf37cf3f0
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-09 00:45:40 +0200

    Imp: getter pagination index

--- src/store/app.ts
+++ src/store/app.ts
@@ -45,6 +45,9 @@ export const useAppStore = defineStore('app', {
     },
     getLessons: (state) : Lesson[] => {
       return state.lessons
+    },
+    getCurrentIndexPage: (state) : number => {
+      return state.pagination.page
     }
   }, // Getters => transformations nécessaire avant d'être utiliser dans le code (pas forcément nécessaire dans un premier temps)
   actions:{ // Actions => changer un état => une méthode, JAMAIS CHANGER EN DEHORS DE CES METHODES IMPORTANT
