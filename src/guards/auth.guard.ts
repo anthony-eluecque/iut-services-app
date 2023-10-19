d30d8cdbaa314ca1888c21ca30f33ca4cfea13b5
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-19 17:09:34 +0200

    Feat: clean authguard

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,12 +1,23 @@
+import { ResponseData, Routes, extractData, fetchData } from "@/api";
 import router from "@/router";
 import { useUserStore } from "@/store"
+import { User } from "@/types";
+import { RouteLocationNormalized, NavigationGuardNext } from 'vue-router';
 
-export const authGuard = () => {
-  const UserStore = useUserStore();
 
-  if (UserStore.isLogged == false) {
-    router.push('/login');
+export const authGuard = async (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+): Promise<void> => {
+  try {
+    const userStore = useUserStore();
+    const userData : ResponseData<User> = await fetchData(Routes.USERS + '/auth');
+    userStore.setUser(extractData(userData));
+
+    next();
+  } catch (error) {
+    console.error('Erreur dans l\'authGuard :', error);
+    next('/error');
   }
-
-  return true;
-}
+};
