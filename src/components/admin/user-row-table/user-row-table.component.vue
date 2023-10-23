d2b279535fc16c4c0f394f44253afc06ce1bf8e0
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-23 17:31:47 +0200

    Temp : user row

---
+++ src/components/admin/user-row-table/user-row-table.component.vue
@@ -0,0 +1,14 @@
+<template>
+    <tr>
+        <td>Synave</td>
+        <td>Rémi</td>
+        <td>adressemail@gmail.com</td>
+        <td></td>
+    </tr>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
