57958e2b9d3f7fd78702fff76a9bb3d34bfd3724
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-23 17:31:07 +0200

    Feat: first version of userstable

---
+++ src/components/admin/users-table/users-table.component.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="table-wrap">
+
+        <table class="table">
+            <thead class="thead">
+                <tr>
+                    <th>Nom</th>
+                    <th>Prénom</th>
+                    <th>Adresse Mail</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <userRowTableComponent v-for="(i) in 5"/>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+
+<script setup lang="ts">
+import userRowTableComponent from '../user-row-table/user-row-table.component.vue';
+</script>
