0da509c4eee0f50f69677327bb9ae26eb040ff97
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-24 18:11:49 +0200

    Feat: add user button

---
+++ src/components/admin/add-user/add-user.component.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="add-action">
+        <v-btn
+            height="55px"
+            append-icon="mdi-plus"
+            text="Ajouter un enseignement"
+            color="primary"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
