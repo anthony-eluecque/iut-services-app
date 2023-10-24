dc64d22733b8a969c3d55b33a549d48969289a47
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-24 18:11:09 +0200

    Feat: search bar

---
+++ src/components/admin/search-bar/search-bar.component.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-form @submit.prevent="search" class="container-criterias">
+        <v-text-field
+          hide-details
+          class="field-id"
+          label="Nom"
+          variant="outlined"
+          clearable
+          persistent-clear
+        />
+        <v-text-field
+          hide-details
+          class="field-id"
+          label="Prénom"
+          variant="outlined"
+          clearable
+          persistent-clear
+        />
+        <v-text-field
+          hide-details
+          class="field-id"
+          label="Adresse mail"
+          variant="outlined"
+          clearable
+          persistent-clear
+        />
+    </v-form>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
