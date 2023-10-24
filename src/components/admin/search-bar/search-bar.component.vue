6b5f64db8eaecf721d55285e8fd9dc152bcd4583
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-10-24 19:26:01 +0200

    Imp: SCSS + add Rows for responsive

--- src/components/admin/search-bar/search-bar.component.vue
+++ src/components/admin/search-bar/search-bar.component.vue
@@ -1,30 +1,49 @@
 <template>
-    <v-form @submit.prevent="search" class="container-criterias">
-        <v-text-field
-          hide-details
-          class="field-id"
-          label="Nom"
-          variant="outlined"
-          clearable
-          persistent-clear
-        />
-        <v-text-field
-          hide-details
-          class="field-id"
-          label="Prénom"
-          variant="outlined"
-          clearable
-          persistent-clear
-        />
-        <v-text-field
-          hide-details
-          class="field-id"
-          label="Adresse mail"
-          variant="outlined"
-          clearable
-          persistent-clear
-        />
-    </v-form>
+    <v-container fluid class="container-fields">
+        <v-row>
+            <v-col cols="3">
+            <v-text-field
+                hide-details
+                class="field-id"
+                label="Nom"
+                variant="outlined"
+                clearable
+                persistent-clear
+            />
+        </v-col>
+        <v-col cols="3">
+            <v-text-field
+                hide-details
+                class="field-id"
+                label="Prénom"
+                variant="outlined"
+                clearable
+                persistent-clear
+            />
+        </v-col>
+        <v-col cols="3">
+            <v-text-field
+                hide-details
+                class="field-id"
+                label="Adresse mail"
+                variant="outlined"
+                clearable
+                persistent-clear
+            />
+        </v-col>
+        <v-col cols="3">
+            <v-btn
+                type="submit"
+                color="primary"
+                class="search-button"
+                append-icon="mdi-magnify"
+                block
+            >
+                Filtrer
+            </v-btn>
+        </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup lang="ts">
--- src/pages/admin/Admin.page.vue
+++ src/pages/admin/Admin.page.vue
@@ -4,10 +4,21 @@
             <h2>Gestion des utilisateurs</h2>
         </v-container>   
         
-        <div class="container-actions">
-            <searchBar/>
-            <addUser/>
-        </div>
+        <v-container fluid class="container-actions">
+            <v-row no-gutters>
+                <v-col cols="6">
+                    <searchBar/>
+
+                </v-col>
+                <v-col cols="2">
+
+                </v-col>
+                <v-col cols="4">
+                    <addUser/>
+
+                </v-col>
+            </v-row>
+        </v-container>
 
         <UsersTable/>
 
