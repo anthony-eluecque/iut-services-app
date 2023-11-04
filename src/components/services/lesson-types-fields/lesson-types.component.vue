0e2aa7ce3f55871495cb125ae6ae658542e51b9d
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-05 00:47:50 +0100

    Feat: lesson-type component

---
+++ src/components/services/lesson-types-fields/lesson-types.component.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-container fluid class="pl-0 pr-0 container-types-modal">
+        <v-row no-gutters>
+            <v-col cols="6">
+                <v-select  
+                    v-model="name"
+                    variant="outlined"
+                    label="Type de cours"
+                    :items="items"
+                    hide-details
+                />
+            </v-col>
+            <v-col cols="1"></v-col>
+            <v-col cols="5">
+                <v-text-field
+                    label="Nombre de cours"
+                    variant="outlined"
+                    hide-details
+                    v-model="hours"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+import { onMounted,ref,Ref } from 'vue';
+
+const items = ['CM','TP','TD']
+
+const props = defineProps({
+    name: {
+      type: String, 
+      required: true, 
+      default : ''
+    },
+    amountHours : {
+        type : Number,
+        required: true,
+        default: 0
+    }
+});
+
+const name : Ref<string> = ref(props.name)
+const hours : Ref<Number> = ref(props.amountHours)
+
+
+</script>
+
