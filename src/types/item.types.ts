26369f72745cf2155d2d1a5a9faa6c55a78301d8
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-05 00:49:29 +0100

    Feat: itemTypes

--- src/types/item.types.ts
+++ src/types/item.types.ts
@@ -1,13 +1,20 @@
 import { IdEntity } from "./entity.types";
 import { Lesson } from "./lesson.types";
 import { Service } from "./service.types";
-import { Teacher } from "./teacher.types";
 
 export type Item = {
-    amountHours : number
     type : string
     lesson? : Lesson
     service? : Service
+    lessonTypes : lessonTypes[]    
 } & IdEntity
 
 
+type lessonTypes = {
+    amountHours : number,
+    lessonType : lessonType
+} & IdEntity
+
+type lessonType = {
+    name : string
+} & IdEntity
