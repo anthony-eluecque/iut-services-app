d000f6be89f448b897cf1d5a189872d4d15f8f10
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-06 19:12:48 +0100

    Fix: SCSS

--- src/components/services/searchbar-services/searchbar-services.component.vue
+++ src/components/services/searchbar-services/searchbar-services.component.vue
@@ -13,70 +13,75 @@ initializeComponent();
 </script>
 
 <template>
-    <v-form @submit.prevent="search" class="container-criteres">
-        <v-text-field
-          v-model="id"
-          hide-details
-          class="field-id"
-          label="Id"
-          variant="outlined"
-          clearable
-          @click:clear="id = ''"
-          persistent-clear
-        />
+    <v-form @submit.prevent="search" class="form-container">
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="id"
+              class="field-id"
+              label="Id"
+              variant="outlined"
+              clearable
+              @click:clear="id = ''"
+              persistent-clear
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="nom"
+              class="field-text"
+              label="Nom"
+              variant="outlined"
+              @click:clear="nom = ''"
+              clearable
+              persistent-clear
+            />
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="prenom"
+              class="field-text"
+              label="Prénom"
+              variant="outlined"
+              @click:clear="prenom = ''"
+              clearable
+              persistent-clear
+            />
+          </v-col>
 
-        <v-text-field
-          v-model="nom"
-          hide-details
-          class="field-text"
-          label="Nom"
-          variant="outlined"
-          @click:clear="nom = ''"
-          clearable
-          persistent-clear
-        />
-
-        <v-text-field
-          v-model="prenom"
-          hide-details
-          class="field-text"
-          label="Prénom"
-          variant="outlined"
-          @click:clear="prenom = ''"
-          clearable
-          persistent-clear
-        />
-
-        <v-text-field
-          v-model="ressource"
-          hide-details
-          class="field-text"
-          label="Ressource"
-          variant="outlined"
-          @click:clear="ressource = ''"
-          clearable
-          persistent-clear
-        />
-
-        <v-text-field
-          v-model="libelle"
-          hide-details
-          class="field-text"
-          label="Libéllé"
-          variant="outlined"
-          @click:clear="libelle = ''"
-          clearable
-          persistent-clear
-        />
-        <v-btn
-            type="submit"
-            color="primary"
-            class="search-button"
-            append-icon="mdi-magnify"
-            block
-        >
-            Filtrer
-        </v-btn>
+          <v-col>
+            <v-text-field
+              v-model="ressource"
+              class="field-text"
+              label="Ressource"
+              variant="outlined"
+              @click:clear="ressource = ''"
+              clearable
+              persistent-clear
+            />            
+          </v-col>
+          <v-col>
+            <v-text-field
+              v-model="libelle"
+              class="field-text"
+              label="Libéllé"
+              variant="outlined"
+              @click:clear="libelle = ''"
+              clearable
+              persistent-clear
+            />
+          </v-col>
+          <v-col>
+            <v-btn
+              type="submit"
+              color="primary"
+              append-icon="mdi-magnify"
+              class="search-button"
+            >
+                Filtrer
+            </v-btn>
+          </v-col>
+        </v-row>
     </v-form>
         
 </template>
