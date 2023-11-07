7899a55cf136889397fcae66c2884a0fe9673d84
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-11-07 13:31:29 +0100

    feat: add user deletion function

--- src/api/index.ts
+++ src/api/index.ts
@@ -110,6 +110,14 @@ export const deleteTeacher = async (
   return await performRequest('delete',`${route}/${id}`,undefined,config);
 }
 
+export const deleteUser = async(
+  route : Routes|string, 
+  id: string, 
+  config = {}
+) => {
+  return await performRequest('delete',`${route}/${id}`,undefined,config);
+}
+
 export const updateData = async <T>(
     route : Routes|string, 
     data: any, 
