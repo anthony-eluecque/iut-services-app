d8fd6062b2fb25363e931ff619e76b7d11f11243
LucieLeFur <lucie.lefur2003@gmail.com>
2023-11-08 19:14:10 +0100

    sup compte

---
+++ src/components/setting/delete-account/delete-account.component.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-row justify="center">
+      <v-dialog v-model="dialog" width="auto">
+        <v-card>
+          <v-card-title class="text-h5">
+            Suppression de votre compte
+          </v-card-title>
+          <v-card-text
+            >Voulez vous vraiment supprimez votre compte ?
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="red-darken-1" variant="text" @click="$emit('deleteAccount')">
+              Oui
+            </v-btn>
+            <v-btn color="blue-darken-1" variant="text" @click="$emit('closeDialog')">
+              Non
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+    let dialog = ref(true)
+  </script>
+  
