2a2e62e22eb5eff09c80173d055d5285777f412b
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-09 10:01:42 +0100

    Fix: legal Notice SASS

--- src/pages/legal-notice/Legal-notice.page.vue
+++ src/pages/legal-notice/Legal-notice.page.vue
@@ -1,24 +1,32 @@
 <template>
     <section class="container d-flex flex-column">
-        <div class="container-title d-flex">
+        <v-container fluid class="container-title-notices primary bg-background-container"  >
             <h2>Mentions Légales</h2>
-            <img src="../../assets/IUT.png" alt="Logo de l'IUT" />
-        </div>
+            <div class="container-img-title">
+                <img src="../../assets/IUT.png" alt="Logo de l'IUT" />
+            </div>
+        </v-container>
   
         <p> Conformément aux dispositions de la loi n°2004 575 du 21 juin 2004 pour la confiance en économie numérique . Il est important de préciser aux utilisateurs de l’application “IUT service” l’identité des différents intervenants dans le cadre de sa réalisation et sa suivie. </p>
     
         <h3>Edition du projet</h3>
         <div class="container-edition-img d-flex">
             <p>Le Fur Lucie : cheffe de projet <br />Bernard Laetitia : cheffe de projet <br /> Cumzain Houssam: chef de projet <br /> Eluecque Anthony: chef de projet </p>
-            <img src="../../assets/JrCanDev.png" alt="Logo de JrCanDev" />
+            <div class="container-img">
+                <img src="../../assets/JrCanDev.png" alt="Logo de JrCanDev" />
+            </div>
         </div>
   
-      <h3> Sources:</h3>
-      <p>Material Design Icons</p>
-  
-      <h3> Données personnelles</h3>
-      <p>Aucun traitement de données personnelles n’est effectué au sein de l’application puisque aucune donnée n’est stockée</p>
-        
+        <div class="container-sources">
+            <h3> Sources:</h3>
+            <p>Material Design Icons</p>
+        </div>
+
+        <div class="container-rgpd">
+            <h3> Données personnelles</h3>
+            <p>Aucun traitement de données personnelles n’est effectué au sein de l’application puisque aucune donnée n’est stockée</p>
+        </div>
+
     </section>
   
   </template>
