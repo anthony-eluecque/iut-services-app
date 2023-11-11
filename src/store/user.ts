32b189f1f5951b819dca4edbdbb90fa2439b98e5
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-11 19:56:51 +0100

    Feat: docstring user store

--- src/store/user.ts
+++ src/store/user.ts
@@ -7,18 +7,37 @@ interface UserStoreState {
   user : User|null
 }
 
+/**
+ * Définit l'état initial de la gestion des utilisateurs.
+ *
+ * @returns {UserStoreState} - L'état initial de la gestion des utilisateurs.
+ */
 const initStore = () => {
   return {
     user : null
   }
 }
 
+/**
+ * Gère l'état des utilisateurs avec des getters et des actions.
+ */
 export const useUserStore = defineStore('user', {
   state: (): UserStoreState => initStore(),
   getters : {
+    /**
+     * Récupère l'utilisateur actuellement enregistré.
+     *
+     * @param {UserStoreState} state - L'état actuel de la gestion des utilisateurs.
+     * @returns {User} - L'utilisateur actuellement enregistré.
+     */
     getUser : (state) : User => state.user as User,
   },
   actions : {
+    /**
+     * Modifie l'utilisateur actuellement enregistré.
+     *
+     * @param {User} newUser - Le nouvel utilisateur à enregistrer.
+     */
     setUser(newUser : User) : void {
       this.user = newUser;
     },
