b678cdc6f969a4942354f40a38a345dc87c27ae1
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-11 19:56:27 +0100

    Fix : remove useless part

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,41 +3,6 @@ import { authGuard } from '@/guards/auth.guard'
 import { changePasswordGuard } from '@/guards/change-password.guard'
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 
-// const routes: RouteRecordRaw[] = [
-  
-//   {
-//     path: '/home',
-//     name: 'Home',
-//     beforeEnter: authGuard,
-//     component: () => import('@/pages/home/Home.page.vue'),
-//   },
-//   {
-//     path: '/services',
-//     name : 'Services',
-//     beforeEnter: authGuard,
-//     component: () => import('@/pages/services/Services.page.vue'),
-//   },
-//   {
-//     path: '/login',
-//     name : 'Login',
-//     component: () => import('@/pages/login/Login.page.vue'),
-//   },
-//   {
-//     path : '/services/:id',
-//     name : 'ServicesDetails',
-//     component: () => import('@/pages/details/Details.page.vue'),
-//     props: route => ({
-//       id: route.params.id,
-//       itemToOpenJSON: route.query.itemToOpenJSON ? JSON.parse(route.query.itemToOpenJSON as string) : null,
-//     }),
-//   },
-//   {
-//     path: '/directory',
-//     name : 'Directory',
-//     component: () => import('@/pages/directory/Directory.page.vue'),
-//   },
-// ]
-
 const routes: RouteRecordRaw[] = [
   {
     path : '/',
