76d59dca767fe5e722726e310abfd61dea03c57a
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-11-15 12:43:24 +0100

    Fix: legal notice

--- src/pages/legal-notice/Legal-notice.page.vue
+++ src/pages/legal-notice/Legal-notice.page.vue
@@ -12,12 +12,20 @@
         >
       </div>
     </v-container>
-  
+
     <p> Conformément aux dispositions de la loi n°2004 575 du 21 juin 2004 pour la confiance en économie numérique . Il est important de préciser aux utilisateurs de l’application “IUT service” l’identité des différents intervenants dans le cadre de sa réalisation et sa suivie. </p>
     
-    <h3>Edition du projet</h3>
+    <h2>1. Edition du projet</h2>
     <div class="container-edition-img d-flex">
-      <p>Le Fur Lucie : cheffe de projet <br>Bernard Laetitia : cheffe de projet <br> Cumzain Houssam: chef de projet <br> Eluecque Anthony: chef de projet </p>
+      <p>
+      Eluecque Anthony: Chef de projet & Développeur
+        <br>
+      Le Fur Lucie : Développeur 
+        <br>
+        Bernard Laetitia : Développeur 
+        <br> Cumzain Houssam: Développeur 
+
+      </p>
       <div class="container-img">
         <img
           src="../../assets/JrCanDev.png"
@@ -25,15 +33,54 @@
         >
       </div>
     </div>
-  
-    <div class="container-sources">
-      <h3> Sources:</h3>
-      <p>Material Design Icons</p>
-    </div>
 
-    <div class="container-rgpd">
-      <h3> Données personnelles</h3>
-      <p>Aucun traitement de données personnelles n’est effectué au sein de l’application puisque aucune donnée n’est stockée</p>
+    <section>
+        <h2>2. Collecte de données personnelles</h2>
+        <p>Les données personnelles collectées sur ce site sont uniquement destinées à un usage interne. En aucun
+            cas ces données ne seront cédées ou vendues à des tiers.</p>
+        <p>Conformément aux dispositions de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers
+            et aux libertés, l'utilisateur dispose d'un droit d'accès, de modification, de rectification et de
+            suppression des données le concernant. Pour exercer ce droit, l'utilisateur peut contacter le
+            responsable du site à l'adresse suivante : contact@exemple.com</p>
+    </section>
+
+    <section>
+        <h2>3. Cookies</h2>
+        <p>Ce site utilise des cookies pour améliorer l'expérience de navigation et fournir des fonctionnalités
+            supplémentaires. Les cookies sont des fichiers stockés sur votre ordinateur qui permettent une
+            personnalisation du contenu. Vous pouvez désactiver l'utilisation des cookies en modifiant les
+            paramètres de votre navigateur.</p>
+    </section>
+
+    <section>
+        <h2>4. Responsabilité</h2>
+        <p>Le propriétaire du site ne peut être tenu responsable pour tout dommage direct ou indirect résultant
+            de l'accès au site ou de son utilisation.</p>
+    </section>
+
+    <section>
+        <h2>5. RGPD - Règlement Général sur la Protection des Données</h2>
+        <p>Conformément au RGPD, vous disposez des droits suivants sur vos données personnelles :</p>
+        <ul style="margin-left: 50px;">
+            <li>Droit d'accès : Vous pouvez obtenir des informations sur les données personnelles que nous
+                détenons vous concernant.</li>
+            <li>Droit de rectification : Vous pouvez demander la correction de vos données personnelles si elles
+                sont inexactes.</li>
+            <li>Droit à l'effacement : Vous pouvez demander la suppression de vos données personnelles.</li>
+            <li>Droit à la limitation du traitement : Vous pouvez demander la limitation du traitement de vos
+                données personnelles.</li>
+            <li>Droit à la portabilité des données : Vous pouvez demander la transmission de vos données personnelles à
+                un autre responsable du traitement.</li>
+            <li>Droit d'opposition : Vous pouvez vous opposer au traitement de vos données personnelles.</li>
+        </ul>
+        <p>Pour exercer ces droits, veuillez nous contacter à l'adresse suivante : contact@exemple.com</p>
+    </section>
+
+    <div class="container-sources">
+      <h2>6. Sources:</h2>
+      <a href="https://www.figma.com/community/plugin/740272380439725040/material-design-icons">Material Design Icons</a>
+      <br>
+      <a href="https://vuetifyjs.com/en/">Vuetify</a>
     </div>
   </section>
 </template>
